03113bed0fa1b82386a5f361d4c3f1f68048e642
sfsmikroskil <sfsmikroskil@gmail.com>
2026-09-08 05:58:27 +0700

    Update bot07.py

--- bot07.py
+++ bot07.py
@@ -105,7 +105,7 @@ async def main(nama, email, c):
 
 if __name__ == "__main__":
     jumlah = 200
-    mulaiDari = 300
+    mulaiDari = 500
     print("Mulai...")
     for i in range (mulaiDari, mulaiDari+jumlah):
         contact = contacts[i]
