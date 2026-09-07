a1c7c98fea5ebe48a0946a94e41e1f1ef7c0137f
sfsmikroskil <sfsmikroskil@gmail.com>
2026-09-07 16:52:27 +0700

    Update bot04.py

--- bot04.py
+++ bot04.py
@@ -51,14 +51,14 @@ async def main(nama, email, c):
 #Regis
             await page.click("button[type='submit']")
             await page.wait_for_timeout(10000)
-            await page.screenshot(path=f"{c}_1login.png")
+            await #page.screenshot(path=f"{c}_1login.png")
 
             # 4. Tombol lewati
             await page.mouse.click(352, 1007)
             await page.wait_for_timeout(100)
             await page.mouse.click(352, 1007)
             await page.wait_for_timeout(7000) #memuat hall dengan banner
-            await page.screenshot(path=f"{c}_2lewati.png")
+            await #page.screenshot(path=f"{c}_2lewati.png")
 
             #Close banner
             #await page.wait_for_timeout(5000)
@@ -68,7 +68,7 @@ async def main(nama, email, c):
             await page.wait_for_timeout(1000)
             
             #await page.wait_for_timeout(2000)
-            await page.screenshot(path=f"{c}_3banner.png")
+            await #page.screenshot(path=f"{c}_3banner.png")
 
             
             # 6. Masukk Hall
@@ -76,19 +76,19 @@ async def main(nama, email, c):
             # klik hall
             await page.mouse.click(277, 654)
             await page.wait_for_timeout(5000)
-            await page.screenshot(path=f"{c}_4hall.png")
+            await #page.screenshot(path=f"{c}_4hall.png")
             # 7. Filter booth
             await page.mouse.click(420, 30)
             await page.wait_for_timeout(2000)
-            await page.screenshot(path=f"{c}_5filter.png") #setelah klik dropdown
+            await #page.screenshot(path=f"{c}_5filter.png") #setelah klik dropdown
             await page.keyboard.type("imigra")
             await page.wait_for_timeout(1000)
-            await page.screenshot(path=f"{c}_6booth.png")
+            await #page.screenshot(path=f"{c}_6booth.png")
             await page.keyboard.press("Enter")
             # Sudah masuk boot
 
             await page.wait_for_timeout(3000)
-            await page.screenshot(path=f"{c}_7booth_imigrasi.png") #berhasil filter
+            await #page.screenshot(path=f"{c}_7booth_imigrasi.png") #berhasil filter
             await page.mouse.click(470, 830) #info
             #await page.mouse.click(357, 624) #video
             #await page.mouse.click(170, 662) #poster kiri
@@ -104,15 +104,15 @@ async def main(nama, email, c):
         await browser.close()
 
 if __name__ == "__main__":
-    jumlah = 100
-    mulaiDari = 1000
+    jumlah = 500
+    mulaiDari = 0
     print("Mulai...")
     for i in range (mulaiDari, mulaiDari+jumlah):
         contact = contacts[i]
         nama = contact["nama"]
         #nama = nama[3:-3]
         email = contact["email"]
-        #email = sisip(email, "_d")
+        email = sisip(email, "004")
         c = i
         #print(f"Proses: {nama} ({email})")
         asyncio.run(main(nama, email, c))
